8a0143c98ecaed6110dac0c1d7a686c4870c720f
Jesenko Mehmedbasic <jesenko@mehmedbasic.dk>
2019-06-09 12:43:37 +0200

    New method

--- main.c
+++ main.c
@@ -8,46 +8,59 @@
 #include <avr/power.h>
 #include <util/delay.h>
 
+struct interval {
+  uint16_t min;
+  uint16_t max;
+  uint16_t numerator;
+  uint16_t denominator;
+};
+
+const uint16_t PWM_CONSTANT = F_CPU / 25000;
+const struct interval INTERVALS[] = {
+    {0, 800, 1, 5}, {800, 850, 2, 5}, {850, 900, 3, 5}, {900, 950, 4, 5}, {950, 1024, 5, 5}};
+
 void pwmBegin(void) {
-  clock_prescale_set(clock_div_1);
+  clock_prescale_set(clock_div_2);
 
   DDRC = 1 << 7;
 
-  TC4H = (F_CPU / 25000 - 1) >> 8;
-  OCR4C = (F_CPU / 25000 - 1) & 0xFF;
+  TC4H = (PWM_CONSTANT - 1) >> 8;
+  OCR4C = (PWM_CONSTANT - 1) & 0xFF;
 
   TCCR4A = (1 << COM4A1) | (1 << PWM4A);
   TCCR4B = (1 << CS40);
 }
 
 void setDutyCycle(uint16_t numerator, uint16_t denominator) {
-  TC4H = (F_CPU / 25000 * numerator / denominator) >> 8;
-  OCR4A = (F_CPU / 25000 * numerator / denominator) & 0xFF;
+  TC4H = (PWM_CONSTANT * numerator / denominator) >> 8;
+  OCR4A = (PWM_CONSTANT * numerator / denominator) & 0xFF;
 }
-
-uint16_t tick = 0;
-uint16_t ones = 0;
 
 int main(void) {
   DDRD = 0;
+
+  ADMUX = 0;                             // reference is AREF
+  ADCSRA = (1 << ADPS2) | (1 << ADPS1);  // ADC prescaler 1/64
+  ADCSRA |= 1 << ADEN;
 
   pwmBegin();
   setDutyCycle(1, 5);
 
   while (1) {
-    for (tick = 0; tick < 1000; ++tick) {
-      if (PIND & (1 << PD4)) {
-        ones++;
-      }
-
-      _delay_us(1);
+    ADCSRA |= 1 << ADSC;
+    while ((ADCSRA & (1 << ADSC)) != 0) {
+      // wait for ADC
     }
 
-    setDutyCycle(ones, tick);
+    uint8_t low = ADCL;
+    uint8_t high = ADCH;
+    uint16_t value = (high << 8) | low;
 
-    tick = 0;
-    ones = 0;
+    for (uint8_t i = 0; i < 5; ++i) {
+      struct interval current = INTERVALS[i];
+      if (current.min <= value && current.max > value) {
+        setDutyCycle(current.numerator, current.denominator);
+      }
+    }
   }
-
-  return 0;
 }
